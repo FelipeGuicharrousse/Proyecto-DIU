b3824cfbf5eefce758d6f771be8a9b7051398532
FelipeGuicharrousse <felipe.guicharrousse@usm.cl>
2023-10-20 17:32:39 -0300

    ✨ Added images

--- src/components/formWithImage.js
+++ src/components/formWithImage.js
@@ -10,60 +10,61 @@ import {
     Stack,
     Image,
   } from '@chakra-ui/react';
-  import { useNavigate } from 'react-router-dom';
-  
-  export default function SplitScreen() {
-    const navigate = useNavigate();
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const role = event.target.role.value;
-      navigate(`/becas/${role}`);
-    };
-  
-    return (
-      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-        <Flex p={8} flex={1} align={'center'} justify={'center'}>
-          <Stack spacing={4} w={'full'} maxW={'md'}>
-            <Heading fontSize={'2xl'}>Ingresa Tu Cuenta Estudiante USM</Heading>
-            <form onSubmit={handleSubmit}>
-              <FormControl id="role">
-                <FormLabel>Rol (Formato: XXXXXXXXX-X)</FormLabel>
-                <Input type="text" name="role" />
-              </FormControl>
-              <FormControl id="email">
-                <FormLabel>Correo Institucional</FormLabel>
-                <Input type="email" />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel>Contraseña</FormLabel>
-                <Input type="password" />
-              </FormControl>
-              <Stack spacing={6}>
-                <Stack
-                  direction={{ base: 'column', sm: 'row' }}
-                  align={'start'}
-                  justify={'space-between'}>
-                  <Checkbox>Recuérdame</Checkbox>
-                  <Text color={'blue.500'}>¿Olvidaste la Contraseña?</Text>
-                </Stack>
-                <Button type="submit" colorScheme={'blue'} variant={'solid'}>
-                  Ingresar
-                </Button>
+import { useNavigate } from 'react-router-dom';
+import imagen from '../images/imagen-central.jpeg'
+
+export default function SplitScreen() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const role = event.target.role.value;
+    navigate(`/becas/${role}`);
+  };
+
+  return (
+    <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+      <Flex p={8} flex={1} align={'center'} justify={'center'}>
+        <Stack spacing={4} w={'full'} maxW={'md'}>
+          <Heading fontSize={'2xl'}>Ingresa Tu Cuenta Estudiante USM</Heading>
+          <form onSubmit={handleSubmit}>
+            <FormControl id="role">
+              <FormLabel>Rol (Formato: XXXXXXXXX-X)</FormLabel>
+              <Input type="text" name="role" />
+            </FormControl>
+            <FormControl id="email">
+              <FormLabel>Correo Institucional</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Contraseña</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={6}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Recuérdame</Checkbox>
+                <Text color={'blue.500'}>¿Olvidaste la Contraseña?</Text>
               </Stack>
-            </form>
-          </Stack>
-        </Flex>
-        <Flex flex={1}>
-          <Image
-            alt={'Login Image'}
-            objectFit={'cover'}
-            src={
-              'https://noticias.usm.cl/wp-content//uploads/2020/12/imagen-central.jpeg'
-            }
-          />
-        </Flex>
-      </Stack>
-    );
-  }
+              <Button type="submit" colorScheme={'blue'} variant={'solid'}>
+                Ingresar
+              </Button>
+            </Stack>
+          </form>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={'Login Image'}
+          objectFit={'cover'}
+          src={
+            imagen
+          }
+        />
+      </Flex>
+    </Stack>
+  );
+}
   
